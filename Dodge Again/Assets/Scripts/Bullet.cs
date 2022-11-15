e9b9cd7ab8027e3f3287bc0b70b548aad0dd8b15
imaramm <c94444@naver.com>
2022-11-15 14:40:48 +0900

    first

---
+++ Dodge Again/Assets/Scripts/Bullet.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody bulletRigidbody;
+
+    private void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+
+        bulletRigidbody.velocity = transform.forward * speed;
+
+        Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PlayerController playerController = other.GetComponent<PlayerController>();
+
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
+
+
